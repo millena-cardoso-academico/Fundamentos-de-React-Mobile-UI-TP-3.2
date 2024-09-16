84d627f595f252e76570f6a0fe28be588e5c90a5
millena-cardoso-academico <millena.santos@al.infnet.edu.br>
2024-09-16 16:06:53 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('nav-menu-visible');
+});
+
